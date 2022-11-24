3e768273068617f022da5951a074a37a965b9935
Stuffy33 <eric96an@gmail.com>
2022-11-24 23:17:45 +0000

    new models for posts

--- posts/models.py
+++ posts/models.py
@@ -7,21 +7,27 @@ class Post(models.Model):
     Post model, related to 'owner', i.e. a User instance.
     Default image set so that we can always reference image.url.
     """
-    image_filter_choices = [
-        ('_1977', '1977'),
-        ('brannan', 'Brannan'),
-        ('earlybird', 'Earlybird'),
-        ('hudson', 'Hudson'),
-        ('inkwell', 'Inkwell'),
-        ('lofi', 'Lo-Fi'),
-        ('kelvin', 'Kelvin'),
-        ('normal', 'Normal'),
-        ('nashville', 'Nashville'),
-        ('rise', 'Rise'),
-        ('toaster', 'Toaster'),
-        ('valencia', 'Valencia'),
-        ('walden', 'Walden'),
-        ('xpro2', 'X-pro II')
+    # image_filter_choices = [
+    #     ('_1977', '1977'),
+    #     ('brannan', 'Brannan'),
+    #     ('earlybird', 'Earlybird'),
+    #     ('hudson', 'Hudson'),
+    #     ('inkwell', 'Inkwell'),
+    #     ('lofi', 'Lo-Fi'),
+    #     ('kelvin', 'Kelvin'),
+    #     ('normal', 'Normal'),
+    #     ('nashville', 'Nashville'),
+    #     ('rise', 'Rise'),
+    #     ('toaster', 'Toaster'),
+    #     ('valencia', 'Valencia'),
+    #     ('walden', 'Walden'),
+    #     ('xpro2', 'X-pro II')
+    # ]
+    clothing_choices = [
+        ('warm', 'Warm'),
+        ('comfy ', 'Comfy'),
+        ('swimming', 'Swimming'),
+        ('fancy', 'Fancy'),
     ]
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -31,8 +37,13 @@ class Post(models.Model):
     image = models.ImageField(
         upload_to='images/', default='../default_post_rgq6aq', blank=True
     )
-    image_filter = models.CharField(
-        max_length=32, choices=image_filter_choices, default='normal'
+    # image_filter = models.CharField(
+    #     max_length=32, choices=image_filter_choices, default='normal'
+    # )
+    clothing = models.CharField(
+        max_length=32,
+        choices=clothing_choices,
+        default='comfy',
     )
 
     class Meta:
